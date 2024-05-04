2185df13ab5986800b88028f583552d398f8539f
Austin Perry <austinjperry22@gmail.com>
2024-05-04 02:13:02 -0600

    added user routes

---
+++ controllers/usersController.js
@@ -0,0 +1,129 @@
+const User = require('../models/User')
+const Note = require('../models/Note')
+// const asyncHandler = require ('express-async-handler')
+const bcrypt = require('bcrypt')
+
+// @desc Get all users
+// @route GET /users
+// @access Private
+const getAllUsers = async (req, res, next) =>{
+    try {
+        const users = await User.find().select('-password').lean()
+        if(!users){
+            return res.status(400).json( {message: 'no users found'})
+        }
+        res.json(users)
+    } catch (error) {
+        next(error)
+    }
+}
+
+// @desc create user
+// @route POST /users
+// @access Private
+const createUser = async (req, res, next) =>{
+    try {
+        const { username, password, roles } = req.body
+        if(!username || !password || !Array.isArray(roles) || !roles.length){
+            return res.status(400).json({message: 'All fields required'})
+        }
+        const duplicate = await User.findOne({ username }).lean().exec()
+
+        if(duplicate){
+            return res.status(409).json({message: "duplicate username"})
+        }
+
+        const hashedPwd = await bcrypt.hash(password, 10)
+
+        const userObject = { username, "password": hashedPwd, roles}
+
+        const user = await User.create(userObject)
+
+        if (user){
+            res.status(201).json({ message: `New user ${username} created`})
+        }else{
+            res.status(400).json({message: 'Invalid user data received'})
+        }
+    } catch (error) {
+        next(error)
+    }
+    
+}
+
+// @desc update a user
+// @route Put /users
+// @access Private
+const updateUser = async (req, res, next) =>{
+    try {
+        const {id, username, roles, active, password} = req.body
+
+        if (!id || !username || !Array.isArray(roles) || !roles.length || typeof active != 'boolean'){
+            return res.status(400).json({message: 'all fields are required'})
+        }
+
+        const user = await User.findById(id).exec()
+
+        if(!user){
+            return res.status(400).json({message: 'user not found'})
+        }
+
+        const duplicate = await User.findOne({ username }).lean().exec()
+
+        if (duplicate && duplicate?._id.toString() !== id){
+            return res.status(409).json({message: 'Duplicate Username'})
+        }
+        user.username = username
+        user.roles = roles
+        user.active = active
+
+        if(password) {
+            // hash password
+            user.password = await bcrypt.hash(password,10)
+        }
+
+        const updatedUser = await user.save()
+
+        res.json({message: `${updatedUser.username} updated`})
+    } catch (error) {
+        next(error)
+    }
+    
+}
+
+// @desc delete a user
+// @route Delete /users
+// @access Private
+const deleteUser = async (req, res, next) =>{
+    try {
+        const { id } = req.body
+        if (!id){
+            return res.status(400).json({message:"User id required"})
+        }
+
+        const notes = await Note.findOne({ user: id}).lean().exec()
+        if(notes){
+            return res.status(400).json({ message: 'User has assigned notes'})
+        }
+
+        const user = await User.findById(id).exec()
+
+        if(!user){
+            return res.status(400).json({message : 'user not found'})
+        }
+
+        const result = await user.deleteOne()
+
+        const reply = `Username ${result.username} with ID ${result._id} deleted`
+        res.json(reply)
+    } catch (error) {
+        next(error)
+    }
+    
+}
+
+module.exports = {
+    getAllUsers,
+    createUser,
+    updateUser,
+    deleteUser
+}
--- models/Note.js
+++ models/Note.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-const AutoIncrement = require('mongoose-sequence')
+// const AutoIncrement = require('mongoose-sequence')
 
 const noteSchema = new mongoose.Schema(
     {
@@ -25,10 +25,10 @@ const noteSchema = new mongoose.Schema(
     }
 )
 
-noteSchema.plugin(AutoIncrement, {
-    inc_field: 'ticket',
-    id: 'ticketNums',
-    start_seq: 500
-})
+// noteSchema.plugin(AutoIncrement, {
+//     inc_field: 'ticket',
+//     id: 'ticketNums',
+//     start_seq: 500
+// })
 
 module.exports = mongoose.model("Note", noteSchema)
