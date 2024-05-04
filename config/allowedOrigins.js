c714875a8b9fe1e756d135c1e481748f87a092f0
Austin Perry <austinjperry22@gmail.com>
2024-05-04 00:29:10 -0600

    added cors

---
+++ config/allowedOrigins.js
@@ -0,0 +1,6 @@
+const allowedOrigins = [
+    'http://localhost:8080',
+    // other domains here
+]
+
+module.exports = allowedOrigins
--- server.js
+++ server.js
@@ -4,13 +4,19 @@ const path = require('path')
 const { logger } = require('./middleware/logger')
 const errorHandler = require('./middleware/errorHandler')
 const cookieParser = require('cookie-parser')
+const cors = require('cors')
+const corsOptions = require('./config/corsOptions')
 const PORT = process.env.PORT || 3000
 
 app.use(logger)
 
+app.use(cors(corsOptions))
+
 app.use(express.json())
 
 app.use(cookieParser())
+
+app.use(cors)
 
 app.use('/', express.static(path.join(__dirname, 'public')))
 
